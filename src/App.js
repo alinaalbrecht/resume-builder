c95497e3881ea3e21d444aeca34956ce0a4e634a
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-07-22 13:27:38 +0200

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -2,38 +2,26 @@ import "./App.scss";
 import { Experience } from "./Components/Experience";
 import { Header } from "./Components/Header";
 import { Sidebar } from "./Components/Sidebar";
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      preview: false,
-    };
-    this.toggleMode = this.toggleMode.bind(this);
-  }
-  toggleMode() {
-    this.setState({
-      preview: !this.state.preview,
-    });
-  }
-  render() {
-    return (
-      <div
-        className={`App ${
-          this.state.preview ? "resume--preview" : "resume--edit"
-        }`}
-      >
-        <div className="App__title">Resume Builder</div>
-        <Header />
-        <Sidebar />
-        <Experience />
-        <button className="button--mode" onClick={this.toggleMode}>
-          {this.state.preview ? "Edit Mode" : "Preview Mode"}
-        </button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [preview, setPreview] = useState(false);
+
+  const toggleMode = () => {
+    setPreview(!preview);
+  };
+
+  return (
+    <div className={`App ${preview ? "resume--preview" : "resume--edit"}`}>
+      <div className="App__title">Resume Builder</div>
+      <Header />
+      <Sidebar />
+      <Experience />
+      <button className="button--mode" onClick={toggleMode}>
+        {preview ? "Edit Mode" : "Preview Mode"}
+      </button>
+    </div>
+  );
+};
 
 export default App;
