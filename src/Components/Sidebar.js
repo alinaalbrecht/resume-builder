29121640723fba0905255734b990d9dae5aeb316
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-07-22 15:37:27 +0200

    refactor sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,101 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { SidebarSection } from "./SidebarSection";
 import uniqid from "uniqid";
 
-export class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sections: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.addSection = this.addSection.bind(this);
-    this.deleteSection = this.deleteSection.bind(this);
-  }
+export const Sidebar = (props) => {
+  const [sections, setSections] = useState([]);
 
-  deleteSection(e) {
-    const index = this.state.sections
+  const deleteSection = (e) => {
+    const index = sections
       .map((section) => section.name)
       .indexOf(e.target.name);
 
-    let updatedSections = Object.assign({}, this.state);
-    updatedSections.sections.splice(index, 1);
+    setSections([...sections.slice(0, index), ...sections.slice(index + 1)]);
+  };
 
-    this.setState({
-      updatedSections,
-    });
-  }
+  const addSection = () => {
+    setSections([
+      ...sections,
+      {
+        edit: true,
+        title: "",
+        bullets: [],
+        name: uniqid(),
+      },
+    ]);
+  };
 
-  addSection() {
-    let updatedSections = Object.assign({}, this.state);
-    updatedSections.sections.push({
-      edit: true,
-      title: "",
-      bullets: [],
-      name: uniqid(),
-    });
-    this.setState({
-      updatedSections,
-    });
-  }
-
-  handleChange(e) {
+  const handleChange = (e) => {
     let value = e.target.value;
     if (e.target.id === "bullets") {
       value = value.split(",");
     }
-    const index = this.state.sections
+    const index = sections
       .map((section) => section.name)
       .indexOf(e.target.name);
     const field = e.target.id;
-    let updatedSections = Object.assign({}, this.state);
-    updatedSections.sections[index][field] = value;
-    this.setState({
-      updatedSections,
-    });
-  }
+    let updatedSections = [...sections];
+    updatedSections[index][field] = value;
+    setSections(updatedSections);
+  };
 
-  toggleEdit(e) {
+  const toggleEdit = (e) => {
     e.preventDefault();
-    const index = this.state.sections
+    const index = sections
       .map((section) => section.name)
       .indexOf(e.target.name);
 
-    let updatedSections = Object.assign({}, this.state);
-    updatedSections.sections[index].edit =
-      !updatedSections.sections[index].edit;
-    this.setState({
-      updatedSections,
-    });
-  }
-  render() {
-    return (
-      <aside className="sidebar">
-        {this.state.sections.length === 0 && (
-          <p className="description">
-            <em>
-              Sidebar -- Add any extra relevant information here, including
-              skills
-            </em>
-          </p>
-        )}
-        {this.state.sections.map((section) => (
-          <SidebarSection
-            edit={section.edit}
-            title={section.title}
-            bullets={section.bullets}
-            handleChange={this.handleChange}
-            toggleEdit={this.toggleEdit}
-            name={section.name}
-            deleteSection={this.deleteSection}
-            key={section.name}
-          />
-        ))}
-        <button className="button--add" onClick={this.addSection}>
-          Add Section
-        </button>
-      </aside>
-    );
-  }
-}
+    let updatedSections = [...sections];
+    updatedSections[index].edit = !updatedSections[index].edit;
+    setSections(updatedSections);
+  };
+
+  return (
+    <aside className="sidebar">
+      {sections.length === 0 && (
+        <p className="description">
+          <em>
+            Sidebar -- Add any extra relevant information here, including skills
+          </em>
+        </p>
+      )}
+      {sections.map((section) => (
+        <SidebarSection
+          edit={section.edit}
+          title={section.title}
+          bullets={section.bullets}
+          handleChange={handleChange}
+          toggleEdit={toggleEdit}
+          name={section.name}
+          deleteSection={deleteSection}
+          key={section.name}
+        />
+      ))}
+      <button className="button--add" onClick={addSection}>
+        Add Section
+      </button>
+    </aside>
+  );
+};
