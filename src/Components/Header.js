54cd7eada649250dccbbd1bfe345b49f303244d7
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-07-22 15:49:34 +0200

    refactor header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,110 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
-export class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      edit: true,
-      inputs: [
-        {
-          type: "text",
-          id: "name",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "email",
-          id: "email",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "tel",
-          id: "phone",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "text",
-          id: "street",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "text",
-          id: "city",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "number",
-          id: "zip",
-          value: "",
-          key: uniqid(),
-        },
-        {
-          type: "text",
-          id: "country",
-          value: "",
-          key: uniqid(),
-        },
-      ],
-    };
+export const Header = () => {
+  const [edit, setEdit] = useState(true);
+  const [inputs, setInputs] = useState([
+    {
+      type: "text",
+      id: "name",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "email",
+      id: "email",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "tel",
+      id: "phone",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "text",
+      id: "street",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "text",
+      id: "city",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "number",
+      id: "zip",
+      value: "",
+      key: uniqid(),
+    },
+    {
+      type: "text",
+      id: "country",
+      value: "",
+      key: uniqid(),
+    },
+  ]);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.toggleEdit = this.toggleEdit.bind(this);
-  }
-  handleChange(e) {
-    const index = this.state.inputs
-      .map((input) => input.id)
-      .indexOf(e.target.id);
-    let updatedInputs = Object.assign({}, this.state);
-    updatedInputs.inputs[index].value = e.target.value;
-    this.setState({
-      updatedInputs,
-    });
-  }
+  const handleChange = (e) => {
+    const index = inputs.map((input) => input.id).indexOf(e.target.id);
+    let updatedInputs = [...inputs];
+    updatedInputs[index].value = e.target.value;
+    setInputs(updatedInputs);
+  };
 
-  toggleEdit(e) {
+  const toggleEdit = (e) => {
     e.preventDefault();
-    this.setState((state) => ({
-      edit: !state.edit,
-    }));
-  }
+    setEdit(!edit);
+  };
 
-  render() {
-    return (
-      <header className="header">
-        {this.state.edit && (
-          <form className="header__form">
-            {this.state.inputs.map((input) => (
-              <div key={input.key} className="form__input">
-                <label htmlFor={input.id}>{input.id}</label>
-                <input
-                  id={input.id}
-                  type={input.type}
-                  value={input.value}
-                  onChange={this.handleChange}
-                />
-              </div>
-            ))}
-            <button className="button--edit" onClick={this.toggleEdit}>
-              submit
-            </button>
-          </form>
-        )}
-        {!this.state.edit && (
-          <div className="header__display">
-            <h1>{this.state.inputs[0].value}</h1>
-            {this.state.inputs.slice(1).map((input) => (
-              <p key={input.key}>{input.value}</p>
-            ))}
-            <button className="button--edit" onClick={this.toggleEdit}>
-              Edit
-            </button>
-          </div>
-        )}
-      </header>
-    );
-  }
-}
+  return (
+    <header className="header">
+      {edit && (
+        <form className="header__form">
+          {inputs.map((input) => (
+            <div key={input.key} className="form__input">
+              <label htmlFor={input.id}>{input.id}</label>
+              <input
+                id={input.id}
+                type={input.type}
+                value={input.value}
+                onChange={handleChange}
+              />
+            </div>
+          ))}
+          <button className="button--edit" onClick={toggleEdit}>
+            submit
+          </button>
+        </form>
+      )}
+      {!edit && (
+        <div className="header__display">
+          <h1>{inputs[0].value}</h1>
+          {inputs.slice(1).map((input) => (
+            <p key={input.key}>{input.value}</p>
+          ))}
+          <button className="button--edit" onClick={toggleEdit}>
+            Edit
+          </button>
+        </div>
+      )}
+    </header>
+  );
+};
